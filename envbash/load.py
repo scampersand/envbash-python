7bdcbf59a954206c1fc34294105fa8492c5030dd
Aron Griffis <aron@scampersand.com>
2017-02-07 20:39:40 -0500

    local env overrides env.bash by default
    This changes the default policy to something that should be more
    expected and less surprising: local environment overrides env.bash, and
    variables aren't removed by default.
    
    If env.bash should override, for example in a deployed environment, then
    the caller should pass override=True and probably remove=True to
    load_envbash().

--- envbash/load.py
+++ envbash/load.py
@@ -4,13 +4,17 @@ import os
 from .read import read_envbash
 
 
-def load_envbash(envbash, into=os.environ, remove=True, **kwargs):
+def load_envbash(envbash, into=os.environ, override=False, remove=False, **kwargs):
     """
     Load ``envbash`` into ``into`` (default ``os.environ``).
     """
     loaded = read_envbash(envbash, **kwargs)
     if loaded is not None:
-        into.update(loaded)
         if remove:
             for k in set(into) - set(loaded):
                 del into[k]
+        if override:
+            into.update(loaded)
+        else:
+            for k in set(loaded) - set(into):
+                into[k] = loaded[k]
--- tests/test_load.py
+++ tests/test_load.py
@@ -5,16 +5,15 @@ import pytest
 from envbash.load import load_envbash
 
 
-def test_load_add_remove(mocker):
-    # original environ lacks A but has C and E
-    if 'A' in os.environ:
-        del os.environ['A']  # pragma: no cover
-    os.environ['C'] = 'D'
-    os.environ['E'] = 'F'
+def _setup(mocker):
+    os.environ['A'] = 'A'
+    os.environ['B'] = 'B'
+    os.environ['C'] = 'C'
+    if 'D' in os.environ:
+        del os.environ['D']  # pragma: no cover
 
-    # loaded environ overrides A, inherits C and removes E
-    loaded = dict(os.environ, A='B')
-    del loaded['E']
+    loaded = dict(os.environ, A='a', D='d')
+    del loaded['B']
 
     # mock the call to read_envbash to return our result.
     # frankly this is harder than an env.bash tmpfile but properly unit tests
@@ -22,20 +21,53 @@ def test_load_add_remove(mocker):
     read = mocker.patch('envbash.load.read_envbash')
     read.return_value = loaded
 
+
+def test_load_no_override_no_remove(mocker):
+    _setup(mocker)
+
     # the first argument doesn't matter since read_envbash is mocked
     load_envbash('')
 
-    assert os.environ['A'] == 'B'  # loaded
-    assert os.environ['C'] == 'D'  # inherited
-    assert 'E' not in os.environ   # removed
+    assert os.environ['A'] == 'A'  # NOT overridden
+    assert os.environ['B'] == 'B'  # NOT removed
+    assert os.environ['C'] == 'C'  # inherited
+    assert os.environ['D'] == 'd'  # loaded
 
 
-def test_load_no_remove(mocker):
-    read = mocker.patch('envbash.load.read_envbash')
-    read.return_value = {}  # would remove everything
-    orig = dict(os.environ)
-    load_envbash('', remove=False)
-    assert dict(os.environ) == orig
+def test_load_override_no_remove(mocker):
+    _setup(mocker)
+
+    # the first argument doesn't matter since read_envbash is mocked
+    load_envbash('', override=True)
+
+    assert os.environ['A'] == 'a'  # overridden
+    assert os.environ['B'] == 'B'  # NOT removed
+    assert os.environ['C'] == 'C'  # inherited
+    assert os.environ['D'] == 'd'  # loaded
+
+
+def test_load_no_override_remove(mocker):
+    _setup(mocker)
+
+    # the first argument doesn't matter since read_envbash is mocked
+    load_envbash('', remove=True)
+
+    assert os.environ['A'] == 'A'  # NOT overridden
+    assert 'B' not in os.environ   # removed
+    assert os.environ['C'] == 'C'  # inherited
+    assert os.environ['D'] == 'd'  # loaded
+
+
+def test_load_override_remove(mocker):
+    _setup(mocker)
+
+    # the first argument doesn't matter since read_envbash is mocked
+    load_envbash('', override=True, remove=True)
+
+    assert os.environ['A'] == 'a'  # overridden
+    assert 'B' not in os.environ   # removed
+    assert os.environ['C'] == 'C'  # inherited
+    assert os.environ['D'] == 'd'  # loaded
 
 
 def test_load_into(mocker):
