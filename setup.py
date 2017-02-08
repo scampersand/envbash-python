70a9ec76d4485d9f6af044eb35d26b957f03703e
Aron Griffis <aron@scampersand.com>
2017-02-07 20:45:52 -0500

    bump to 1.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='envbash',
-    version='1.0.0',
+    version='1.1.0',
     description='Source env.bash script to update environment',
     long_description=long_description,
     url='https://github.com/scampersand/envbash',
