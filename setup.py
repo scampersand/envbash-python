6417c63a7c806ac54198b230577d0dda7a0582e7
Aron Griffis <aron@scampersand.com>
2026-03-20 10:17:32 -0400

    chore: bump supported python to 3.14

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
     ],
     keywords='bash environment environ env'.split(),
     packages=find_packages(exclude=['tests']),
